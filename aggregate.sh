a790e780e8d3db5f0b68c87320af1aad5a03e0ae
Ethan Li <ethanopensource@gmail.com>
2017-10-16 13:52:14 +0000

    add another group of results

--- aggregate.sh
+++ aggregate.sh
@@ -1,25 +1,33 @@
 
 #This script is to aggreate results for further analysis
 
-if [ $# -ne 2 ] 
+if [ $# -lt 2 ] 
     then
-        echo "USAGE: $0 ROOT_DIR REGREX"
+        echo "USAGE: $0 ROOT_DIR REGREX SKIP_NR"
         exit
 fi
 
 ROOT_DIR=$1
 REGREX=$2
 
+if [ $# -eq 3 ]
+    then
+        SKIP_NR=$3
+    else
+        SKIP_NR=6
+fi
+
 echo "ROOT: " ${ROOT_DIR}
 echo "REGREX: " ${REGREX}
+echo "SKIP_NR: " ${SKIP_NR}
 echo "rate(tuple/s)	mean(ms)	99%ile(ms)	99.9%ile(ms)	cores	mem(MB)"
 
 for file in "$ROOT_DIR"/*
 do
   case "$file" in 
   *${REGREX}*)
-#echo $file
-     awk 'NR >=6 { 
+echo $file
+     awk -v skip="$SKIP_NR" 'NR >=skip { 
                    count++;
                    rate += $3;
                    mean += $4;
@@ -29,10 +37,14 @@ do
                    mem += $8
                  } 
                  END {
-                   print rate/count  "\t"  mean/count  "\t" ile99/count  "\t" ile999/count  "\t" core/count  "\t" mem/count
+                    print rate/count  "\t"  mean/count  "\t" ile99/count  "\t" ile999/count  "\t" core/count  "\t" mem/count
                  }' $file
 
     ;;
    esac  
 done
+
+
+
+
 
